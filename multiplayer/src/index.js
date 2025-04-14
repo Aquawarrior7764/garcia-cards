957f68927aeb1393bce91abaee329201ef9fda51
Aquawarrior7764 <157011952+Aquawarrior7764@users.noreply.github.com>
2025-04-14 00:35:36 -1000

    Update index.js

--- multiplayer/src/index.js
+++ multiplayer/src/index.js
@@ -2,13 +2,18 @@ import Phaser from "phaser";
 import Game from "./scenes/game"
 
 const config = {
-    type: Phaser.AUTO,
-    parent: "phaser-example",
-    width: 1280,
-    height: 780,
-    scene: [
-        Game
-    ]
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  backgroundColor: '#000000',
+  parent: 'game-container',
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+  scene: [GameScene]
 };
+
+const game = new Phaser.Game(config);
 
 const game = new Phaser.Game(config);
