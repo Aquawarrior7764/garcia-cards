7cdc4ee030d707bc3e6cabc1373c90a4205b81be
Aquawarrior7764 <157011952+Aquawarrior7764@users.noreply.github.com>
2025-04-14 10:09:25 -1000

    Update memoryGame.js

--- memoryGame.js
+++ memoryGame.js
@@ -16,14 +16,6 @@ let zeroSecond = true;
 
 const intervalID = window.setInterval(timerDisplay, 1000);
 
-const ENCRYPTED_CARD_MAP = {
-  a3x7kg: "gcu1", k9mz2p: "gcu2", v2y8wd: "gcu3", j1qb5t: "gcu4", x4le9m: "gcu5",
-  d0rn6a: "guc1", f7w3cy: "guc2", m3pz8k: "guc3", p9xu2d: "guc4",
-  h8lj3s: "grc1", u5mn9q: "grc2", z2rx0c: "grc3",
-  n4cy7b: "gec1", q7dk1r: "gec2",
-  t6wy9z: "glc1", e1bj5v: "glc2"
-};
-
 const RARITY_MAP = {
   gcu1: "common", gcu2: "common", gcu3: "common", gcu4: "common", gcu5: "common",
   guc1: "uncommon", guc2: "uncommon", guc3: "uncommon", guc4: "uncommon",
